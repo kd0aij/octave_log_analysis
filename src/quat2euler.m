6e52fc5a1951e9d9b5f1d254a8dac2fdfbd72d06
Mark Whitehorn <kd0aij@gmail.com>
2018-11-05 17:46:37 -0700

    add support for quaternion object input

--- src/quat2euler.m
+++ src/quat2euler.m
@@ -1,5 +1,13 @@
 function [roll pitch yaw] = quat2euler(q)
-  # input q is an array [w x y z] 
+  # input q is an array [w x y z] or quaternion object: q.w q.i q.j q.k
+  if isa(q,'quaternion') 
+    qs = q;
+    q = [];
+    q(1) = qs.w;
+    q(2) = qs.i;
+    q(3) = qs.j;
+    q(4) = qs.k;
+  endif
   # q = w + xi + yj + zk
   # result is xyz-fixed Euler angles in degrees
   roll  = rad2deg(atan2(2*(q(1)*q(2) + q(3)*q(4)), 1 - 2*(q(2)*q(2) + q(3)*q(3))));
