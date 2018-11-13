fd56ee4d12faeee67140e2eb1b735f8b629d1b85
Mark Whitehorn <kd0aij@gmail.com>
2018-11-13 14:23:29 -0700

    update script to run from /home/markw/git/kd0aij

--- src/buildWorkspace.m
+++ src/buildWorkspace.m
@@ -6,8 +6,7 @@ run temp.m
 
 # generate a workspace containing the desired data
 data = alignData(POS, GPS, ATT, IMU, NKF1, NKQ1);
+segments = segment_maneuvers2(2, 0, data);
 
 # and save to temp.work
 save 'temp.work'
-
-segments = segment_maneuvers2(2, 0, data);
--- src/dflog2octave.sh
+++ src/dflog2octave.sh
@@ -7,13 +7,12 @@ echo "input logfile:" $2
 rm temp.m
 cd $1
 echo "running mavtomfile.py --types POS,GPS,ATT,IMU,NKF1,NKQ1 "$2".bin"
-mavtomfile.py --types POS,GPS,ATT,IMU,NKF1,NKQ1 "$2".bin
-echo "renaming m_"$2".m to temp.m"
-mv "m_"$2".m" ~/sf_markw/git/kd0aij/octave_log_analysis/src/temp.m
-cd ~/sf_markw/git/kd0aij/octave_log_analysis/src
+mavtomfile.py --types POS,GPS,ATT,IMU,NKF1,NKQ1 "$2".bin 
+cp "$2".m ~/git/kd0aij/octave_log_analysis/src/temp.m
+cd ~/git/kd0aij/octave_log_analysis/src
 cat buildWorkspace.m > buildWorkspace_tmp.m
 echo "label='"$2"'" >> buildWorkspace_tmp.m
-echo "label=['log' label(regexp(label,'[^0]')(1):end)]" >> buildWorkspace_tmp.m
+echo "label=[label(regexp(label,'[^0]')(1):end)]" >> buildWorkspace_tmp.m
 echo "plot_segments" >> buildWorkspace_tmp.m
 echo "running flatpak run org.octave.Octave --no-gui buildWorkspace_tmp.m"
 flatpak run org.octave.Octave --no-gui buildWorkspace_tmp.m
