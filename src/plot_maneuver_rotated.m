7a59ef21148321104e22042d33dd3a475e9b1200
Mark Whitehorn <kd0aij@gmail.com>
2021-05-15 17:02:36 -0600

    add plot selection

--- src/plot_maneuver_rotated.m
+++ src/plot_maneuver_rotated.m
@@ -1,6 +1,6 @@
 function plot_maneuver_rotated(startTime, endTime, data,
   mnum=0, label='untitled', origin=[39.8420194 -105.2123333 1808], 
-  rTol=15, posIndex=2, rhdg=106, pThresh=88, plotTitle='')
+  rTol=15, posIndex=2, rhdg=106, whichplots=[0 1 2 3], pThresh=88, plotTitle='')
   
 # rhdg is runway heading: 0 is North, 90 is East
 # latitude is converted to Y axis in meters
@@ -115,7 +115,7 @@ for idx = 2:Nsamp
       # use ground heading to define maneuver plane
       disp("exit from vertical line")
       # set maneuver plane heading to current ground heading
-      mplane.hdg = ghdg(idx) - r2box;
+      mplane.hdg = ghdg(idx);
       mplane.pos = xyzr(idx,:);
       mplane.entry = false;
       disp(sprintf("rotated maneuver heading %3.0f", mplane.hdg));
@@ -132,7 +132,7 @@ for idx = 2:Nsamp
       # on entry to vertical line:
       disp("entry to vertical line")
       # pick aerobatic box heading using previous ground heading
-      mplane.hdg = getManeuverPlane(rhdg, ghdg(idx-1)) - r2box;
+      mplane.hdg = getManeuverPlane(rhdg, ghdg(idx-1));
       mplane.pos = xyzr(idx,:);
       mplane.entry = true;
       mhdg(idx) = mplane.hdg;
@@ -142,7 +142,10 @@ for idx = 2:Nsamp
     endif  
   endif
   # with maneuver plane determined, calculate maneuver roll, pitch and wind correction angle
-  [roll(idx) pitch(idx) wca(idx)] = maneuver_roll_pitch(mhdg(idx)+r2box, quat(idx,:), pThresh);
+  [roll(idx) pitch(idx) wca(idx)] = maneuver_roll_pitch(mhdg(idx), quat(idx,:), pThresh);
+  if abs(wca) > 12
+    disp(sprintf("large wca: %5.1f", wca));
+  endif
   # crosswind is ~ |vENU|*sin(wca): so percentage of earthframe velocity is:
   xwnd(idx) = 100 * abs(sind(wca(idx)));
 endfor
@@ -176,128 +179,130 @@ endfor
 sizes = 4 * ones(length(colors),1);
 
 fignum = mnum * 10;
-figure(fignum++, 'position', [900,100,800,800])
-scatter3(xyzr(:,1), xyzr(:,2), xyzr(:,3), 8, colors);
-axis equal
-grid on
-rotate3d on
-title('ENU xyz')
-xlabel('East')
-ylabel('North')
-zlabel('Alt')
+if any(whichplots == 0)
+  figure(fignum, 'position', [900,100,800,800])
+  scatter3(xyzr(:,1), xyzr(:,2), xyzr(:,3), 8, colors);
+  axis equal
+  grid on
+  rotate3d on
+  title('ENU xyz')
+  xlabel('East')
+  ylabel('North')
+  zlabel('Alt')
 
-# plot maneuver plane: vertical, rotated to maneuver heading
-hold on
-limits=axis();
-x = [0; 0];
-y = [-25; 25];
-#TODO: assign colors to maneuver planes: how to vectorize this?
-cmat = zeros(2,2,3);
-for idx = 1:length(mplanes)
-  # rotate to heading
-  theta = 180 - mplanes(idx).hdg;
-  xyr = [(cosd(theta)*x).-(sind(theta)*y), (sind(theta)*x).+(cosd(theta)*y)];
-  # translate to vehicle position
-  xyr(:,1:2) += mplanes(idx).pos(1:2);
-  [xx yy] = meshgrid(xyr(:,1),xyr(:,2));
-  zz = [-25 -25; 25 25] + mplanes(idx).pos(3);
-  # red for vertical entry, green for exit
-  if mplanes(idx).entry
-    pcolor = red;
-  else
-    pcolor = green;
-  endif
-  for i = 1:3
-    cmat(:,:,i) = pcolor(i);
+  # plot maneuver plane: vertical, rotated to maneuver heading
+  hold on
+  limits=axis();
+  x = [0; 0];
+  y = [-25; 25];
+  #TODO: assign colors to maneuver planes: how to vectorize this?
+  cmat = zeros(2,2,3);
+  for idx = 1:length(mplanes)
+    # rotate to heading
+    theta = 180 - mplanes(idx).hdg + r2box;
+    xyr = [(cosd(theta)*x).-(sind(theta)*y), (sind(theta)*x).+(cosd(theta)*y)];
+    # translate to vehicle position
+    xyr(:,1:2) += mplanes(idx).pos(1:2);
+    [xx yy] = meshgrid(xyr(:,1),xyr(:,2));
+    zz = [-25 -25; 25 25] + mplanes(idx).pos(3);
+    # red for vertical entry, green for exit
+    if mplanes(idx).entry
+      pcolor = red;
+    else
+      pcolor = green;
+    endif
+    for i = 1:3
+      cmat(:,:,i) = pcolor(i);
+    endfor
+    s1 = surf(xx, yy', zz, cmat);
+    set(s1, 'facealpha', 0.1);
   endfor
-  s1 = surf(xx, yy', zz, cmat);
-  set(s1, 'facealpha', 0.1);
-endfor
 
-# rotate to runway heading
-q2runway = rot2q([0 0 1], deg2rad(-r2box));
+  # rotate to runway heading
+  q2runway = rot2q([0 0 1], deg2rad(-r2box));
 
-# plot delta wing planes at intervals
-hold on
-scale = 10;
-gN = 3;
-# 6 xy grid points at [-.1, -1] [.1, -1] [.3, 0] [.1 1] [-.1 1] [-.1 0]
-wx = [-.1 -.1 -.1;
-      .1   .3  .1];
-wy = [-1 0 1;
-      -1 0 1];
-xx = zeros(2,3);
-yy = zeros(2,3);
-zz = zeros(2,3);
-cmat = zeros(2,3,3);
-for idx = 1:10:Nsamp
-  # rotate and translate from body to world NED
-  q = quaternion(quat(idx,1), quat(idx,2), quat(idx,3), quat(idx,4));
-  q = q2runway * q;
-  R = q2rotm(q);
-  for n = 1:3
-    for m = 1:2
-      v = R * (scale * [wx(m,n); wy(m,n); 0]);
-      xx(m,n) =  v(2) + xyzr(idx,1);
-      yy(m,n) =  v(1) + xyzr(idx,2);
-      zz(m,n) = -v(3) + xyzr(idx,3);
-      cmat(m,n,:) = colors(idx,:);
+  # plot delta wing planes at intervals
+  hold on
+  scale = 10;
+  gN = 3;
+  # 6 xy grid points at [-.1, -1] [.1, -1] [.3, 0] [.1 1] [-.1 1] [-.1 0]
+  wx = [-.1 -.1 -.1;
+        .1   .3  .1];
+  wy = [-1 0 1;
+        -1 0 1];
+  xx = zeros(2,3);
+  yy = zeros(2,3);
+  zz = zeros(2,3);
+  cmat = zeros(2,3,3);
+  for idx = 1:10:Nsamp
+    # rotate and translate from body to world NED
+    q = quaternion(quat(idx,1), quat(idx,2), quat(idx,3), quat(idx,4));
+    q = q2runway * q;
+    R = q2rotm(q);
+    for n = 1:3
+      for m = 1:2
+        v = R * (scale * [wx(m,n); wy(m,n); 0]);
+        xx(m,n) =  v(2) + xyzr(idx,1);
+        yy(m,n) =  v(1) + xyzr(idx,2);
+        zz(m,n) = -v(3) + xyzr(idx,3);
+        cmat(m,n,:) = colors(idx,:);
+      end
     end
-  end
-  s1 = surf(xx, yy, zz, cmat);
-endfor
+    s1 = surf(xx, yy, zz, cmat);
+  endfor
 
-# add tail fins at intervals
-hold on
-scale = 10;
-x = [-0.1 0.3;
-     -0.1 0.3];
-z = [0 0;
-     0 0];
-y = [0 0;
-     -0.3 0];
-xx = zeros(2,2);
-yy = zeros(2,2);
-zz = zeros(2,2);
-cmat = zeros(2,2,3);
-for idx = 1:10:Nsamp
-  # rotate and translate from body to world NED
-  q = quaternion(quat(idx,1), quat(idx,2), quat(idx,3), quat(idx,4));
-  q = q * rot2q([1 0 0], pi/2);
-  q = q2runway * q;
-  R = q2rotm(q);
-  for n = 1:2
-    for m = 1:2
-      v = R * (scale * [x(m,n); y(m,n); z(m,n)]);
-      xx(m,n) =  v(2) + xyzr(idx,1);
-      yy(m,n) =  v(1) + xyzr(idx,2);
-      zz(m,n) = -v(3) + xyzr(idx,3);
-      cmat(m,n,:) = colors(idx,:);
+  # add tail fins at intervals
+  hold on
+  scale = 10;
+  x = [-0.1 0.3;
+       -0.1 0.3];
+  z = [0 0;
+       0 0];
+  y = [0 0;
+       -0.3 0];
+  xx = zeros(2,2);
+  yy = zeros(2,2);
+  zz = zeros(2,2);
+  cmat = zeros(2,2,3);
+  for idx = 1:10:Nsamp
+    # rotate and translate from body to world NED
+    q = quaternion(quat(idx,1), quat(idx,2), quat(idx,3), quat(idx,4));
+    q = q * rot2q([1 0 0], pi/2);
+    q = q2runway * q;
+    R = q2rotm(q);
+    for n = 1:2
+      for m = 1:2
+        v = R * (scale * [x(m,n); y(m,n); z(m,n)]);
+        xx(m,n) =  v(2) + xyzr(idx,1);
+        yy(m,n) =  v(1) + xyzr(idx,2);
+        zz(m,n) = -v(3) + xyzr(idx,3);
+        cmat(m,n,:) = colors(idx,:);
+      end
     end
-  end
-  s1 = surf(xx, yy, zz, cmat);
-endfor
+    s1 = surf(xx, yy, zz, cmat);
+  endfor
 
-# save figure
-savefig("3D", label, mnum, 800, 800);
+  # save figure
+  savefig("3D", label, mnum, 800, 800);
+endif
 
-##return
-
-figure(fignum++, 'position', [400,50,1080,400])
-hold on
-plot(tsp,  yaw, 'og', tsp, e_pitch, 'ob',tsp, wrap180(unwrapd(e_roll), rTol), 'oc')
-plot(tsp, mhdg, '*-m', tsp, pitch, '*-k', tsp, wrap180(unwrapd(roll), rTol), '*-r')
-plot(tsp, xwnd, '-k')
-title('Euler RPY vs. maneuver RPY')
-legend(['eyaw'; 'epitch'; 'eroll'; 'mhdg'; 'pitch'; 'roll'; 'xwnd %'])
-axis tight
-grid minor
-# save figure
-savefig("eulerVmp", label, mnum, 1080, 540);
-#fname = sprintf("%s_eulerVmp_%d", label, mnum);
-#disp(sprintf("saving 3D track display: %s", fname));
-#print ([fname ".jpg"], "-S1080,540")
-#hgsave ([fname ".jpg"])
+if any(whichplots == 1)
+  figure(fignum+1, 'position', [400,50,1080,400])
+  hold on
+  plot(tsp,  yaw, 'og', tsp, e_pitch, 'ob',tsp, wrap180(unwrapd(e_roll), rTol), 'oc')
+  plot(tsp, mhdg, '*-m', tsp, pitch, '*-k', tsp, wrap180(unwrapd(roll), rTol), '*-r')
+  plot(tsp, xwnd, '-k')
+  title('Euler RPY vs. maneuver RPY')
+  legend(['eyaw'; 'epitch'; 'eroll'; 'mhdg'; 'pitch'; 'roll'; 'xwnd %'])
+  axis tight
+  grid minor
+  # save figure
+  savefig("eulerVmp", label, mnum, 1080, 540);
+  #fname = sprintf("%s_eulerVmp_%d", label, mnum);
+  #disp(sprintf("saving 3D track display: %s", fname));
+  #print ([fname ".jpg"], "-S1080,540")
+  #hgsave ([fname ".jpg"])
+endif
 
 # plot 5 second time intervals
 thacks = [];
@@ -305,144 +310,149 @@ for idx = 1:125:Nsamp
   thacks = [thacks tsp(idx)];
 endfor
 
-if length(plotTitle) == 0
-  plotTitle = sprintf("roll tolerance: %2.0f", rTol);
+if any(whichplots == 2)
+  if length(plotTitle) == 0
+    plotTitle = sprintf("roll tolerance: %2.0f", rTol);
+  endif
+
+  figure(fignum+2, 'position', [100,100,1600,800])
+  subplot(2,2,1)
+  scatterPlot(xyzr, 1, 2, sizes, colors, blue, [-350 350])
+  title (sprintf("Plan view\n%s", plotTitle))
+  xlabel "east (m)"
+  ylabel "north (m)"
+
+  subplot(2,2,2)
+  scatterPlot(xyzr, 1, 3, sizes, colors, blue, [-350 350])
+  title (sprintf("North elevation"))
+  xlabel "east (m)"
+  ylabel "alt (m)"
+
+  subplot(2,2,3)
+  scatterPlot(xyzr, 2, 3, sizes, colors, blue, [0 300])
+  title (sprintf("East elevation"))
+  xlabel "north (m)"
+  ylabel "alt (m)"
+
+  subplot(2,2,4)
+  plot(tsp, roll, '-r', tsp, pitch, '-k', tsp, yaw, '-m');
+  limits=axis();
+  xoffset = (limits(2)-limits(1))/(length(thacks)*8);
+  yoffset = limits(3) + (limits(4)-limits(3))/80;
+  hold on
+  for idx = 1:length(thacks)
+    plot([thacks(idx) thacks(idx)],limits(3:4),'-b');
+    text(thacks(idx)-xoffset,yoffset,num2str(idx-1))
+  endfor
+  yGrid = [rTol 45 90 180-rTol 180];
+  for idx = 1:length(yGrid)
+    plot([limits(1) limits(2)],[-yGrid(idx),-yGrid(idx)],'-b');
+    plot([limits(1) limits(2)],[ yGrid(idx), yGrid(idx)],'-b');
+  endfor
+
+  xticks(thacks);
+  xlabels={};
+  for idx=1:length(thacks)
+    xlabels(idx) = sprintf("%4.1f",thacks(idx));
+  endfor
+  xticklabels(xlabels);
+  yticks([-yGrid yGrid]);
+  axis([tsp(1),tsp(end),limits(3),limits(4)])
+  grid off
+
+  title (sprintf("roll, pitch, yaw"))
+  xlabel "time"
+  ylabel "degrees)"
+  legend("roll","pitch","yaw","location","northeastoutside")
+
+  # save figure
+  savefig("maneuver", label, mnum, 800, 800);
 endif
-fignum
-figure(fignum++, 'position', [100,100,800,800])
-subplot(2,2,1)
-scatterPlot(xyzr, 1, 2, sizes, colors, blue, [-350 350])
-title (sprintf("Plan view\n%s", plotTitle))
-xlabel "east (m)"
-ylabel "north (m)"
 
-subplot(2,2,2)
-scatterPlot(xyzr, 1, 3, sizes, colors, blue, [-350 350])
-title (sprintf("North elevation"))
-xlabel "east (m)"
-ylabel "alt (m)"
+if any(whichplots == 3)
+  uroll = unwrapd(roll);
+  wuroll = wrap180(uroll, rTol);
 
-subplot(2,2,3)
-scatterPlot(xyzr, 2, 3, sizes, colors, blue, [0 300])
-title (sprintf("East elevation"))
-xlabel "north (m)"
-ylabel "alt (m)"
+  figure(fignum++, 'position', [900,100,800,800])
+  subplot(2,1,1)
+  [ax, h1, h2] = plotyy(tsp, wuroll, tsp, [pitch mhdg]);
+  limits=axis();
+  rline = findobj(ax(1),'linestyle','-');
+  set(rline,"linestyle",'-');
+  set(rline,"marker",'.');
+  set(rline,"markersize",10);
+  axis(ax(1), [tsp(1) tsp(end) -180-rTol 180+rTol]);
+  axis(ax(2), [tsp(1) tsp(end) -180-rTol 180+rTol]);
+  grid(ax(1), 'on')
+  grid(ax(2), 'on')
+  set(ax(1),'ygrid','on')
+  set(ax(2),'ygrid','on')
+  set(ax(2),'xgrid','off')
+  hold on
 
-subplot(2,2,4)
-plot(tsp, roll, 'or', tsp, pitch, '.-k', tsp, yaw, 'om');
-limits=axis();
-xoffset = (limits(2)-limits(1))/(length(thacks)*8);
-yoffset = limits(3) + (limits(4)-limits(3))/80;
-hold on
-for idx = 1:length(thacks)
-  plot([thacks(idx) thacks(idx)],limits(3:4),'-b');
-  text(thacks(idx)-xoffset,yoffset,num2str(idx-1))
-endfor
-yGrid = [rTol 45 90 180-rTol 180];
-for idx = 1:length(yGrid)
-  plot([limits(1) limits(2)],[-yGrid(idx),-yGrid(idx)],'-b');
-  plot([limits(1) limits(2)],[ yGrid(idx), yGrid(idx)],'-b');
-endfor
+  ### highlight abs(pitch) > pThresh
+  ##xxx = find(abs(pitch)>pThresh);
+  ##plot(ax(2), tsp(xxx), pitch(xxx), '.r');
 
-xticks(thacks);
-xlabels={};
-for idx=1:length(thacks)
-  xlabels(idx) = sprintf("%4.1f",thacks(idx));
-endfor
-xticklabels(xlabels);
-yticks([-yGrid yGrid]);
-axis([tsp(1),tsp(end),limits(3),limits(4)])
-grid off
+  # highlight roll error > rTol
+  if length(rollErr) > 0
+    plot(ax(1), tsp(rollErr), roll(rollErr), '.r');
+  endif
+  legend("roll", "rTol", "pitch", "hdg", "location", "northeastoutside");
 
-title (sprintf("roll, pitch, yaw"))
-xlabel "time"
-ylabel "degrees)"
-legend("roll","pitch","yaw")
+  # plot x axis time hacks
+  xoffset = (limits(2)-limits(1))/(length(thacks)*4);
+  yoffset = limits(3) + (limits(4)-limits(3))/40;
+  xticks(ax(1), thacks);
+  xticks(ax(2), []);
+  xlabels={};
+  for idx=2:length(thacks)
+    xlabels(idx) = sprintf("T%i:%4.1f", idx-1, thacks(idx));
+  endfor
+  xticklabels(xlabels);
+  yoff = 0;
+  nwraps = 1;
+  yTicks = [];
+  yGrid = [-180-rTol -180+rTol -135 -90-rTol -90+rTol -45 -rTol ...
+           rTol 45 90-rTol 90+rTol 135 180-rTol 180+rTol];
+  yGridLabels = {'inv', 'inv', 'il45', 'lKE', 'lKE', 'l45', ...
+                 'up', 'up', 'r45', 'rKE', 'rKE', 'ir45', 'inv', 'inv'};
+  yTickLabels = [];
+  for w = 1:nwraps
+    yTicks = [yTicks yoff+yGrid];
+    yTickLabels = [yTickLabels yGridLabels];
+    yoff += 360;
+  endfor
+  yticks(ax(1), yTicks);
+  yticklabels(ax(1), yTickLabels);
 
-# save figure
-savefig("maneuver", label, mnum, 800, 800);
+  yTicks = [-180 -135 -90 -45 0 45 90 135 180];
+  yticks(ax(2), yTicks);
 
-uroll = unwrapd(roll);
-wuroll = wrap180(uroll, rTol);
+  title (sprintf("roll, pitch, maneuver hdg"))
+  xlabel "time"
+  ylabel(ax(1), "roll attitude")
+  ylabel(ax(2), "pitch, hdg degrees")
 
-figure(fignum++, 'position', [900,100,800,800])
-subplot(2,1,1)
-[ax, h1, h2] = plotyy(tsp, wuroll, tsp, [pitch mhdg]);
-rline = findobj(ax(1),'linestyle','-');
-set(rline,"linestyle",'-');
-set(rline,"marker",'.');
-set(rline,"markersize",10);
-axis(ax(1), [tsp(1) tsp(end) -180-rTol 180+rTol]);
-axis(ax(2), [tsp(1) tsp(end) -180-rTol 180+rTol]);
-grid(ax(1), 'on')
-grid(ax(2), 'on')
-set(ax(1),'ygrid','on')
-set(ax(2),'ygrid','on')
-set(ax(2),'xgrid','off')
-hold on
+  subplot(2,1,2)
+  plot(tsp,rad2deg(gv),'.-')
+  limits=axis();
+  xoffset = (limits(2)-limits(1))/(length(thacks)*4);
+  yoffset = limits(3) + (limits(4)-limits(3))/40;
+  #hold on
+  #xticks(thacks);
+  #xticklabels(xlabels);
+  axis([tsp(1),tsp(end),limits(3),limits(4)])
+  grid on
 
-### highlight abs(pitch) > pThresh
-##xxx = find(abs(pitch)>pThresh);
-##plot(ax(2), tsp(xxx), pitch(xxx), '.r');
+  title ("roll, pitch, yaw rates")
+  xlabel "time"
+  ylabel "deg/sec)"
+  legend("roll","pitch","yaw")
 
-# highlight roll error > rTol
-if length(rollErr) > 0
-  plot(ax(1), tsp(rollErr), roll(rollErr), '.r');
+  # save figure
+  savefig("RPY", label, mnum, 800, 800);
 endif
-legend("roll", "rTol", "pitch", "hdg", "location", "northeastoutside");
-
-# plot x axis time hacks
-xoffset = (limits(2)-limits(1))/(length(thacks)*4);
-yoffset = limits(3) + (limits(4)-limits(3))/40;
-xticks(ax(1), thacks);
-xticks(ax(2), []);
-xlabels={};
-for idx=2:length(thacks)
-  xlabels(idx) = sprintf("T%i:%4.1f", idx-1, thacks(idx));
-endfor
-xticklabels(xlabels);
-yoff = 0;
-nwraps = 1;
-yTicks = [];
-yGrid = [-180-rTol -180+rTol -135 -90-rTol -90+rTol -45 -rTol ...
-         rTol 45 90-rTol 90+rTol 135 180-rTol 180+rTol];
-yGridLabels = {'inv', 'inv', 'il45', 'lKE', 'lKE', 'l45', ...
-               'up', 'up', 'r45', 'rKE', 'rKE', 'ir45', 'inv', 'inv'};
-yTickLabels = [];
-for w = 1:nwraps
-  yTicks = [yTicks yoff+yGrid];
-  yTickLabels = [yTickLabels yGridLabels];
-  yoff += 360;
-endfor
-yticks(ax(1), yTicks);
-yticklabels(ax(1), yTickLabels);
-
-yTicks = [-180 -135 -90 -45 0 45 90 135 180];
-yticks(ax(2), yTicks);
-
-title (sprintf("roll, pitch, maneuver hdg"))
-xlabel "time"
-ylabel(ax(1), "roll attitude")
-ylabel(ax(2), "pitch, hdg degrees")
-
-subplot(2,1,2)
-plot(tsp,rad2deg(gv),'.-')
-limits=axis();
-xoffset = (limits(2)-limits(1))/(length(thacks)*4);
-yoffset = limits(3) + (limits(4)-limits(3))/40;
-#hold on
-#xticks(thacks);
-#xticklabels(xlabels);
-axis([tsp(1),tsp(end),limits(3),limits(4)])
-grid on
-
-title ("roll, pitch, yaw rates")
-xlabel "time"
-ylabel "deg/sec)"
-legend("roll","pitch","yaw")
-
-# save figure
-savefig("RPY", label, mnum, 800, 800);
 
 # save workspace
 save([label ".work"])
@@ -454,7 +464,7 @@ function timeHacks(limits, xyzr, c1, c2, color)
   offset = (limits(4)-limits(3))/40;
   hackIndex = 0;
   for idx = 1:125:length(xyzr)
-    scatter(xyzr(idx,c1),xyzr(idx,c2),[10],color)
+    scatter(xyzr(idx,c1),xyzr(idx,c2),20,color)
     text(xyzr(idx,c1)+offset,xyzr(idx,c2)+offset,num2str(hackIndex))
     hackIndex += 1;
   endfor
@@ -482,7 +492,7 @@ endfunction
 function savefig(name, label, mnum, width, height)
   fname = sprintf("%s_%d_%s", label, mnum, name);
   disp(sprintf("saving figure: %s", fname));
-  print ([fname ".jpg"], sprintf("-S%i,%i", width, height))
+  print ([fname ".svg"], sprintf("-S%i,%i", width, height))
   hgsave ([fname ".ofig"]) 
 endfunction
 
