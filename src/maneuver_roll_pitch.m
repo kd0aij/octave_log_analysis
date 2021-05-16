82bac8d70f21a59151a8954691a05b290f9be0b5
Mark Whitehorn <kd0aij@gmail.com>
2021-05-16 16:51:22 -0600

    move comments around and comment out unused code

--- src/maneuver_roll_pitch.m
+++ src/maneuver_roll_pitch.m
@@ -1,22 +1,29 @@
 function [roll pitch wca wca_axis] = maneuver_roll_pitch(rhdg, quat, pThresh)
-  # input q is an array [w x y z] or quaternion object: q.w q.i q.j q.k
+  # given the maneuver heading: rhdg
+  # calculate roll angle as angle between rhdg/earthz plane and body x/y plane
+  # input quat is an array [w x y z] or quaternion object: q.w q.i q.j q.k
   if not(isa(quat,'quaternion') )
     q = quat;
     quat = quaternion(q(1),q(2),q(3),q(4));
   endif
-  # given the maneuver heading: rhdg
-  # calculate roll angle as angle between rhdg/earthz plane and body x/y plane
-  hv = [cosd(rhdg) sind(rhdg) 0];
-  
-  # this hzplane requires maneuvers to lie in a vertical plane
-  hzplane = [-sind(rhdg) cosd(rhdg) 0];
-  
+  # a more general version would allow the maneuver plane to be non-vertical
+  # where mplane is (hv cross earthz) rotated about hv by a roll angle
+##  hv = [cosd(rhdg) sind(rhdg) 0];
+##  hzplane = cross(hv, mplane);
+
   bx = hamilton_product(quat, [1 0 0]);
   bz = hamilton_product(quat, [0 0 1]);
 
-  # a more general version would allow the maneuver plane to be non-vertical
-  # where mplane is (hv cross earthz) rotated about hv by a roll angle
-##  hzplane = cross(hv, mplane);  
+  # this hzplane requires maneuvers to lie in a vertical plane
+  hzplane = [-sind(rhdg) cosd(rhdg) 0];
+  
+##  # reverse heading if necessary
+##  bxdothz = dot(bx, hzplane);
+##  if bxdothz < -.001
+##    rhdg = wrap180(rhdg+180);
+##    hzplane = [-sind(rhdg) cosd(rhdg) 0];
+##    bxdothz = dot(bx, hzplane);
+##  end
 
   # the wind correction angle (WCA) relative to flight path is the
   # angle between body frame x and hzplane
@@ -36,9 +43,6 @@ function [roll pitch wca wca_axis] = maneuver_roll_pitch(rhdg, quat, pThresh)
   pitch = real(rad2deg( asin(2*(fq.w*fq.y - fq.z*fq.x))));
   rolle  = rad2deg(atan2(2*(fq.w*fq.x + fq.y*fq.z), 1 - 2*(fq.x*fq.x + fq.y*fq.y)));
   [roll pitch yaw] = quat2euler(fq);
-##  if abs(pitch) < pThresh
-##    return 
-##  endif
   
   # back out rhdg and pitch
   ryaw = rot2q([0 0 1], deg2rad(-rhdg));
